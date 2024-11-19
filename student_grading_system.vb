755452cf63ac7fbc3321a9920ff3c71ee79b33bf
Nihh-mie <miss.nihlah@gmail.com>
2024-11-19 12:36:01 +0400

    Add files via upload

---
+++ student_grading_system.vb
@@ -0,0 +1,8 @@
+Module Program
+    Sub Main()
+        ' Add code to accept user input
+        Dim number As Integer
+
+        ' Add code to check if the number is even or odd
+    End Sub
+End Module
